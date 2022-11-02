1c56badd99170eb4762f1145b68d9af937424613
Eugene <skrobov.ev@gmail.com>
2022-11-02 11:53:12 +0600

    Add copy ability to Code UI component

--- src/shared/ui/Code/Code.tsx
+++ src/shared/ui/Code/Code.tsx
@@ -1,22 +1,38 @@
+import { useCallback, useRef } from 'react';
 import { PropsWithClassName, PropsWithChildren } from 'shared/types';
 import classNames from 'classnames';
 
+import { Icon } from 'shared/ui/Icon/Icon';
 import { Button, ButtonTheme } from 'shared/ui/Button';
+import CopyIcon from 'shared/assets/icons/copy.svg';
+
 import styles from './Code.module.scss';
 
 type CodeProps = PropsWithClassName & PropsWithChildren;
 
 export const Code = (props: CodeProps) => {
   const { className, children } = props;
+  const codeRef = useRef<HTMLPreElement>(null);
+
+  const onCopy = useCallback(() => {
+    const textContent = codeRef.current?.textContent ?? '';
+    navigator.clipboard.writeText(textContent);
+  }, []);
 
   return (
     <div className={classNames(className, styles.Code)}>
 
-      {/* eslint-disable-next-line i18next/no-literal-string */}
-      <Button theme={ButtonTheme.Outlined} className={styles.copyButton}>Copy</Button>
+      {/* todo: add aria-label or hidden description */}
+      <Button
+        theme={ButtonTheme.Clear}
+        className={styles.copyButton}
+        onClick={onCopy}
+      >
+        <Icon icon={CopyIcon} />
+      </Button>
 
       <code>
-        <pre>
+        <pre ref={codeRef}>
           {children}
         </pre>
       </code>
