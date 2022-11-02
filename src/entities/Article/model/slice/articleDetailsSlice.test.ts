71c3a7c4cda0d32cf0d3826dc9dfaf393e5cc665
Eugene <skrobov.ev@gmail.com>
2022-11-02 12:39:01 +0600

    Add articleDetailsSlice unit tests

--- src/entities/Article/model/slice/articleDetailsSlice.test.ts
+++ src/entities/Article/model/slice/articleDetailsSlice.test.ts
@@ -1,3 +1,61 @@
+import { DeepPartial } from 'shared/types';
+
+import { fetchArticleDetailsData } from '../services/fetchArticleDetailsData/fetchArticleDetailsData';
+import { articleDetailsReducer } from './articleDetailsSlice';
+import { ArticleDetailsSchema } from '../types/articleDetailsSchema';
+import { Article, ArticleType } from '../types/article';
+
+const mockData: Article = {
+  id: '1',
+  title: 'title',
+  subtitle: 'subtitle',
+  img: '',
+  views: 1,
+  type: [ArticleType.It],
+  blocks: [],
+  createdAt: '22.22.2022',
+};
+
 describe('ArticleDetailsSlice', () => {
-  test.todo('write tests');
+  test('should handle fetch article details data service pending', () => {
+    const state: DeepPartial<ArticleDetailsSchema> = {
+      error: 'error',
+      isLoading: false,
+    };
+
+    const action = fetchArticleDetailsData.pending;
+
+    expect(articleDetailsReducer(state as ArticleDetailsSchema, action))
+      .toEqual({
+        isLoading: true,
+      });
+  });
+
+  test('should handle fetch article details data service fulfilled', () => {
+    const state: DeepPartial<ArticleDetailsSchema> = {
+      isLoading: true,
+    };
+
+    const action = fetchArticleDetailsData.fulfilled(mockData, '', mockData.id);
+
+    expect(articleDetailsReducer(state as ArticleDetailsSchema, action))
+      .toEqual({
+        data: mockData,
+        isLoading: false,
+      });
+  });
+
+  test('should handle fetch article details data service rejected', () => {
+    const state: DeepPartial<ArticleDetailsSchema> = {
+      isLoading: true,
+    };
+
+    const action = fetchArticleDetailsData.rejected(null, '', mockData.id, 'error');
+
+    expect(articleDetailsReducer(state as ArticleDetailsSchema, action))
+      .toEqual({
+        error: 'error',
+        isLoading: false,
+      });
+  });
 });
