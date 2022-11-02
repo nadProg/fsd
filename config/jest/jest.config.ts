bb347d60125abd6e29435fee21a11bc09adf206b
Eugene <skrobov.ev@gmail.com>
2022-11-02 22:13:51 +0600

    Fix jest config to import files

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -131,6 +131,8 @@ export default {
 
   moduleNameMapper: {
     '\\.(scss|css)$': 'identity-obj-proxy',
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+       path.resolve(__dirname, 'jestEmptyString.ts'),
     '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
   },
 
