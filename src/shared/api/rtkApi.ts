0560829511824abd20327c94b0455743164498b4
Eugene <skrobov.ev@gmail.com>
2022-11-28 22:41:01 +0600

    Create RTK API

---
+++ src/shared/api/rtkApi.ts
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+import { USER_LOCALSTORAGE_KEY } from 'shared/constants/localStorage';
+
+export const rtkApi = createApi({
+  reducerPath: 'rtkApi',
+  baseQuery: fetchBaseQuery(
+    {
+      baseUrl: __API_URL__,
+      prepareHeaders: (headers) => {
+        const token = localStorage.getItem(USER_LOCALSTORAGE_KEY);
+
+        if (token) {
+          headers.set('Authorization', token);
+        }
+
+        return headers;
+      },
+    },
+  ),
+  endpoints: () => ({}),
+});
