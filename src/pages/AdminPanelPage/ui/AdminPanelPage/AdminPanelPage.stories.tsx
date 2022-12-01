190184696cd669927dec99553a7a599ee271ae9d
Eugene <skrobov.ev@gmail.com>
2022-12-01 13:20:21 +0600

    Update ui tests

--- src/pages/AdminPanelPage/ui/AdminPanelPage/AdminPanelPage.stories.tsx
+++ src/pages/AdminPanelPage/ui/AdminPanelPage/AdminPanelPage.stories.tsx
@@ -1,5 +1,9 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
+import { Theme } from 'shared/providers/ThemeProvider';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
+
 import { AdminPanelPage } from './AdminPanelPage';
 
 export default {
@@ -8,11 +12,18 @@ export default {
   argTypes: {
     backgroundColor: { control: 'color' },
   },
+  decorators: [StoreDecorator({})],
 } as ComponentMeta<typeof AdminPanelPage>;
 
 const Template: ComponentStory<typeof AdminPanelPage> = (args) => <AdminPanelPage {...args} />;
 
-export const Normal = Template.bind({});
-Normal.args = {
-  // args
-};
+export const Light = Template.bind({});
+Light.args = {};
+
+export const Dark = Template.bind({});
+Dark.args = {};
+Dark.decorators = [ThemeDecorator(Theme.Dark)];
+
+export const Orange = Template.bind({});
+Orange.args = {};
+Orange.decorators = [ThemeDecorator(Theme.Orange)];
