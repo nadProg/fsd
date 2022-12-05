c5541c0cfef8a85bc24a092b711d1e1ab93d5879
Eugene <skrobov.ev@gmail.com>
2022-12-05 17:51:10 +0600

    Create useModal hook. WIP

---
+++ src/shared/hooks/useModal/useModal.tsx
@@ -0,0 +1,11 @@
+type UseModalOptions = {
+  onClose?: () => void;
+  isOpen?: boolean;
+  lazy?: boolean;
+  animationDelay?: number
+};
+
+export const useModal = ({
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  animationDelay, lazy, isOpen, onClose,
+}: UseModalOptions) => null;
