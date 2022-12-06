b6345501273dcff0b9e3f3b183a2ba52b27cf102
Eugene <skrobov.ev@gmail.com>
2022-12-06 14:18:25 +0600

    Improve Drawer Animation

--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -1,10 +1,15 @@
+import { useCallback, useEffect } from 'react';
 import classNames from 'classnames';
+import { useDrag } from '@use-gesture/react';
+import { a, useSpring, config } from '@react-spring/web';
 
 import type { PropsWithChildren, PropsWithClassName } from 'shared/types';
 import { Portal } from 'shared/ui/Portal';
 import { Overlay } from 'shared/ui/Overlay';
 
 import styles from './Drawer.module.scss';
+
+const height = window.innerHeight;
 
 type DrawerProps = PropsWithClassName & PropsWithChildren & {
   isOpen?: boolean;
@@ -16,16 +21,72 @@ export const Drawer = ({
   children,
   isOpen,
   onClose,
-}: DrawerProps): JSX.Element => (
-  <Portal>
-    <div className={classNames(className, styles.Drawer, {
-      [styles.opened]: isOpen,
-    })}
-    >
-      <Overlay onClick={onClose} />
-      <div className={styles.content}>
-        {children}
+}: DrawerProps): JSX.Element => {
+  const [{ y }, api] = useSpring(() => ({ y: height }));
+
+  const openDrawer = useCallback(() => {
+    api.start({ y: 0, immediate: false });
+  }, [api]);
+
+  useEffect(() => {
+    if (isOpen) {
+      openDrawer();
+    }
+  }, [api, isOpen, openDrawer]);
+
+  const closeDrawer = useCallback((velocity = 0) => {
+    api.start({
+      y: height,
+      immediate: false,
+      config: {
+        ...config.stiff, velocity,
+      },
+      onResolve: onClose,
+    });
+  }, [api, onClose]);
+
+  const bind = useDrag(
+    ({
+      last,
+      velocity: [, vy],
+      direction: [, dy],
+      movement: [, my],
+      cancel,
+    }) => {
+      if (my < -70) cancel();
+
+      if (last) {
+        if (my > height * 0.5 || (vy > 0.5 && dy > 0)) {
+          closeDrawer();
+        } else {
+          openDrawer();
+        }
+      } else {
+        api.start({ y: my, immediate: true });
+      }
+    },
+    {
+      from: () => [0, y.get()], filterTaps: true, bounds: { top: 0 }, rubberband: true,
+    },
+  );
+
+  const display = y.to((py) => (py < height ? 'block' : 'none'));
+
+  return (
+    <Portal>
+      <div className={classNames(className, styles.Drawer, {
+        [styles.opened]: isOpen,
+      })}
+      >
+        <Overlay onClick={closeDrawer} />
+        <a.div
+          className={styles.content}
+          style={{ display, bottom: `calc(-100vh + ${height - 100}px)`, y }}
+          {...bind()}
+        >
+          {children}
+        </a.div>
       </div>
-    </div>
-  </Portal>
-);
+    </Portal>
+  );
+};
