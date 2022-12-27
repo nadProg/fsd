96fb2483275ef3aaa55b4358cd9c7906ebe65eef
Eugene <skrobov.ev@gmail.com>
2022-12-27 22:44:29 +0600

    Fix Code UI tests

--- src/shared/ui/Code/Code.stories.tsx
+++ src/shared/ui/Code/Code.stories.tsx
@@ -13,22 +13,28 @@ export default {
   },
 } as ComponentMeta<typeof Code>;
 
-const code = `import classNames from 'classnames';
+const code = `
+import classNames from 'classnames';
+
 import { Button, ButtonTheme } from 'shared/ui/Button';
 import styles from './Code.module.scss';
 
 export const Code = (props) => {
   const { className, children } = props;
+  
   return (
     <div className={classNames(className, styles.Code)}>
+    
       {/* eslint-disable-next-line i18next/no-literal-string */}
       <Button theme={ButtonTheme.Outlined} className={styles.copyButton}>Copy</Button>
+      
       <code> 
         <pre>
           {children} 
         </pre>
       </code> 
-    </div>);
+    </div>
+  );
 };`;
 
 const Template: ComponentStory<typeof Code> = (args) => <Code {...args}>{code}</Code>;
